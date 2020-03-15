59f6981930a8f591777c486d5fa9bf68d3ab04bc
j0schiTadamori <j0schitadamori@gmail.com>
2020-03-15 16:41:33 +0300

    Слил проект.

---
+++ Assets/Scripts/C#/UI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UI : MonoBehaviour {
+	
+	public Text scoreTxt;		//The Text component depicting the current score
+	public Text ammoTxt;		//The Text component depicting the player's current ammo
+	public Text moneyTxt;		//The Text component depicting the player's current money
+	public Text waveTxt;		//The Text component depicting the current wave
+	public Text waveTimeTxt;	//The Text component depicting the time left in the wave
+	
+	public Slider healthBar;	//The Slider which shows the players health 
+	public Text hpTxt;			//The Text component depicting the players current and maximum health
+	
+	void Update ()
+	{
+		scoreTxt.text = "Score: " + Game.score;
+		ammoTxt.text = "Ammo: " + Player.ammo;
+		moneyTxt.text = "Money: $" + Player.money;
+		waveTxt.text = "Wave: " + Game.curWave;
+		waveTimeTxt.text = "Time Left: " + Game.curWaveTime;
+		
+		healthBar.value = Player.curHp;
+		healthBar.maxValue = Player.maxHp;
+		hpTxt.text = Player.curHp + "/" + Player.maxHp;
+	}
+}
